ca50f53778f6f9f854858d11dd3c447bb980efd2
Husade <119759512+HusseinAdem@users.noreply.github.com>
2024-06-13 17:00:41 +0200

    declaring variables and code structure

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("Hello world")	
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)	
+	fmt.Printf("we have total of %v tickets and %v are stil available\n", conferenceTickets,remainingTickets)
+	fmt.Println("Get your tickets here")
+
+	var userName string
+	var userTickets int
+	// asking user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName,userTickets)
+
+	
+
 }
